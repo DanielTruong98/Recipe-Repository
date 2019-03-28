28ae6390e17722ffc83c214d509fd36fdf749ee1
Daniel <DanielTruong98@gmail.com>
2019-03-28 15:02:23 -0700

    test

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include "List.h"
+
+
+int main()
+{
+  Recipe Sandwich("Sandwich", 5, 10);
+  /*
+  Sandwich.setRecipeName("Sandwich");
+  Sandwich.setCookingTime(5);
+  Sandwich.setPreparationTime(10);
+  */
+  Sandwich.addIngredient("Bread");
+  Sandwich.addIngredient("Cheese");
+  Sandwich.addIngredient("Ham");
+  Sandwich.getRecipeName();
+  Sandwich.getCookingTime();
+  Sandwich.getPreparationTime();
+  /*
+  Sandwich.getIngredientSize();
+  Sandwich.printIngredients();
+  */
+  return 0;
+}

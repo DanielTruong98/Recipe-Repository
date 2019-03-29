f5e70b1915f112f5a6fc1eeac4a629567840c16e
Daniel <DanielTruong98@gmail.com>
2019-03-29 01:00:36 -0700

    Added multiple functions and partially finished RecipeList class

--- List.h
+++ List.h
@@ -12,11 +12,15 @@ public:
 //setters
   void addIngredient(std::string item);
   void setRecipeName(std::string name);
+  void setCookingTime(int cTime);
+  void setPreparationTime(int pTime);
 
 //getters
   std::string getRecipeName();
   int getCookingTime();
   int getPreparationTime();
+  int getIngredientSize();
+  void printIngredients();
 private:
   std::vector<std::string> ingredients;
   int cookingTime;
@@ -43,6 +47,14 @@ void Recipe::addIngredient(std::string item){
 void Recipe::setRecipeName(std::string name){
   recipeName = name;
 }
+
+void Recipe::setCookingTime(int cTime){
+  cookingTime = cTime;
+}
+
+void Recipe::setPreparationTime(int pTime){
+  preparationTime = pTime;
+}
 //Getters
 std::string Recipe::getRecipeName(){
   std::cout << "Recipe Name: " << recipeName << std::endl;
@@ -59,12 +71,33 @@ int Recipe::getPreparationTime(){
   return preparationTime;
 }
 
+int Recipe::getIngredientSize(){
+  std::cout << "Amount of ingredients: " << ingredients.size() << std::endl;
+  return ingredients.size();
+}
+
+void Recipe::printIngredients(){
+  for (auto i = ingredients.begin(); i != ingredients.end(); i++){
+    std::cout << *i << " ";
+  }
+  std::cout << std::endl;
+}
 
 class RecipeList
 {
 public:
+//Declaration
   RecipeList();
+//setters
+  void addRecipe(std::string rName);
+  void removeRecipe(std::string rName);
+  void changeRecipe(int select);
 
+//getters
+  int recipeAmount();
+  bool listFull();
+  bool listEmpty();
+  //Search through recipes
 private:
   std::vector<Recipe> listRecipe;
   Recipe* current;
@@ -72,8 +105,17 @@ private:
   Recipe* previousRecipe;
 };
 
+//Declaration
 RecipeList::RecipeList(){
   nextRecipe = NULL;
   previousRecipe = NULL;
   current = NULL;
+}
+
+
+//setters
+
+//getters
+int RecipeList::recipeAmount(){
+  return listRecipe.size();
 }
--- test.cpp
+++ test.cpp
@@ -5,20 +5,16 @@
 int main()
 {
   Recipe Sandwich("Sandwich", 5, 10);
-  /*
   Sandwich.setRecipeName("Sandwich");
   Sandwich.setCookingTime(5);
   Sandwich.setPreparationTime(10);
-  */
   Sandwich.addIngredient("Bread");
   Sandwich.addIngredient("Cheese");
   Sandwich.addIngredient("Ham");
   Sandwich.getRecipeName();
   Sandwich.getCookingTime();
   Sandwich.getPreparationTime();
-  /*
   Sandwich.getIngredientSize();
   Sandwich.printIngredients();
-  */
   return 0;
 }
